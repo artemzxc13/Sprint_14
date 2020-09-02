b9a411c564fb07c8e97dfee0863c17c77de4b0e8
artemzxc13 <artyom.kharitonov@27>
2020-09-02 09:23:29 +0300

    new update conft

--- middlewares/err.js
+++ middlewares/err.js
@@ -1,6 +1,6 @@
 const CastError = (err, req, res) => {
   if (err.name === 'DocumentNotFoundError') {
-    return res.status(400).send({ message: `Карточка с _id ${req.params.cardId} не найдена` });
+    return res.status(404).send({ message: `Карточка с _id ${req.params.cardId} не найдена` });
   }
   return res.status(500).send({ message: err.message });
 };
